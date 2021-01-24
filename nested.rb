0f811fe8839c2ce39d8d85dac9523ea3e6a1f2cc
Val_1 S_z <csa.aiden@gmail.com>
2021-01-24 21:10:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-  programmer_hash[:dennis_ritchie][:known_for]
+  programmer_hash[:dennis_ritchie][:languages] = "C"
 end
 
 def adding_matz
@@ -81,7 +81,8 @@ def adding_matz
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
-      }
+      },
+      
     }
 
 end
